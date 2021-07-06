491f1b6b77f37eb3812258fdef16866a749dfb72
Hildebrando Viana Matos <hildocontato@gmail.com>
2021-07-06 15:16:25 -0300

    removing announcement

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,34 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-
-          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <meta name="author" content="Hildebrando Viana Matos" />
+          <meta name="creator" content="Hildebrando" />
+          <meta name="application-name" content="Convert Your Text" />
+          <meta
+            name="description"
+            content="Convert your text simply and quickly to any type of text."
+          />
+          <meta
+            name="keywords"
+            content="convert your text, convert text, text convert, simple convert, fast convert"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta property="og:site_name" content="Convert Your Text" />
+          <meta
+            property="og:title"
+            content="Convert Your Text - Simple and Fast"
+          />
+          <meta property="og:site_name" content="Convert Your Text" />
+          <meta
+            property="og:description"
+            content="Convert your text simply and quickly to any type of text."
+          />
+          <meta property="og:url" content="https://convertyourtext.com" />
+          <meta
+            name="twitter:title"
+            content="Convert Your Text - Simple and Fast"
+          />
+          <link rel="shortcut icon" href="favicon.png" type="image/x-png" />
         </Head>
 
         <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,8 +50,6 @@ export default function Home() {
             />
           </form>
 
-          <section className="announcement"></section>
-
           <section className="contentLorem" id="contentLorem">
             <div className="boxCopy">
               <button
@@ -65,6 +63,177 @@ export default function Home() {
             </div>
             {valueLorem <= 0 || valueLorem > 100 ? (
               <div ref={copyDiv}>
+                <p id="paragraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus laoreet mattis hendrerit. Mauris eget odio id eros
+                  venenatis porttitor sed cursus dui. Ut quis felis vel risus
+                  volutpat tincidunt at a nulla. Quisque nec sollicitudin ipsum.
+                  Aenean ligula magna, porta a eleifend ullamcorper, porta sit
+                  amet arcu. Nullam dictum sodales facilisis. Vestibulum finibus
+                  volutpat nisi at volutpat. Sed id dictum magna, nec ornare
+                  ligula. In eu interdum leo. Pellentesque et convallis diam.
+                  Duis aliquet arcu sit amet orci ultrices, eu blandit nunc
+                  posuere. Vivamus dapibus, ligula consectetur volutpat
+                  elementum, arcu lorem porttitor dui, condimentum efficitur
+                  odio velit ut diam. Ut vitae eros dui. Aliquam laoreet massa
+                  elit, id placerat libero feugiat eu. Suspendisse faucibus
+                  imperdiet congue.
+                </p>
+
+                <p id="paragraph">
+                  Mauris ipsum dui, pretium vitae venenatis eu, blandit quis
+                  nibh. In hac habitasse platea dictumst. In hac habitasse
+                  platea dictumst. Aenean vehicula elementum varius. Curabitur
+                  ut tortor vel enim facilisis consequat sit amet aliquet ante.
+                  Nam feugiat ac turpis eu tincidunt. In sed laoreet sapien,
+                  condimentum tempus nibh. Duis non ipsum augue. Quisque nec
+                  lacus id leo fermentum sagittis. Interdum et malesuada fames
+                  ac ante ipsum primis in faucibus. Aenean malesuada at nibh non
+                  suscipit.
+                </p>
+
+                <p id="paragraph">
+                  Donec tortor felis, mollis in ipsum quis, iaculis rhoncus
+                  quam. Nam odio ex, rutrum sed est et, porttitor scelerisque
+                  diam. Nunc aliquam est vitae sapien elementum facilisis. Ut
+                  pharetra maximus ligula ac tempus. Phasellus luctus luctus
+                  hendrerit. Duis fermentum efficitur felis quis vehicula. Fusce
+                  ut egestas arcu, sed tristique nulla. Praesent orci magna,
+                  scelerisque in orci volutpat, condimentum pulvinar nibh.
+                  Quisque nunc mauris, tempus id ante sit amet, mollis accumsan
+                  tellus. Praesent aliquet lacinia maximus. Etiam semper enim
+                  arcu, at fringilla urna placerat in. Integer nec mollis purus,
+                  sed vehicula purus.
+                </p>
+
+                <p id="paragraph">
+                  Proin sit amet metus nibh. Quisque eleifend velit nec ligula
+                  consequat, at tempor nunc ultricies. Curabitur pulvinar a urna
+                  ut suscipit. Maecenas rutrum mi augue, quis imperdiet purus
+                  suscipit id. Vestibulum tincidunt orci eu blandit tempor.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia curae; In aliquet fermentum elit, sit
+                  amet dictum est laoreet a. Curabitur vulputate fringilla
+                  suscipit. Donec sit amet facilisis nisl. Nulla justo orci,
+                  ullamcorper eu congue vitae, ornare sed nulla. Proin turpis
+                  massa, sagittis in vestibulum quis, interdum nec nulla.
+                  Vivamus molestie eleifend rhoncus.
+                </p>
+
+                <p id="paragraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus laoreet mattis hendrerit. Mauris eget odio id eros
+                  venenatis porttitor sed cursus dui. Ut quis felis vel risus
+                  volutpat tincidunt at a nulla. Quisque nec sollicitudin ipsum.
+                  Aenean ligula magna, porta a eleifend ullamcorper, porta sit
+                  amet arcu. Nullam dictum sodales facilisis. Vestibulum finibus
+                  volutpat nisi at volutpat. Sed id dictum magna, nec ornare
+                  ligula. In eu interdum leo. Pellentesque et convallis diam.
+                  Duis aliquet arcu sit amet orci ultrices, eu blandit nunc
+                  posuere. Vivamus dapibus, ligula consectetur volutpat
+                  elementum, arcu lorem porttitor dui, condimentum efficitur
+                  odio velit ut diam. Ut vitae eros dui. Aliquam laoreet massa
+                  elit, id placerat libero feugiat eu. Suspendisse faucibus
+                  imperdiet congue.
+                </p>
+
+                <p id="paragraph">
+                  Mauris ipsum dui, pretium vitae venenatis eu, blandit quis
+                  nibh. In hac habitasse platea dictumst. In hac habitasse
+                  platea dictumst. Aenean vehicula elementum varius. Curabitur
+                  ut tortor vel enim facilisis consequat sit amet aliquet ante.
+                  Nam feugiat ac turpis eu tincidunt. In sed laoreet sapien,
+                  condimentum tempus nibh. Duis non ipsum augue. Quisque nec
+                  lacus id leo fermentum sagittis. Interdum et malesuada fames
+                  ac ante ipsum primis in faucibus. Aenean malesuada at nibh non
+                  suscipit.
+                </p>
+
+                <p id="paragraph">
+                  Donec tortor felis, mollis in ipsum quis, iaculis rhoncus
+                  quam. Nam odio ex, rutrum sed est et, porttitor scelerisque
+                  diam. Nunc aliquam est vitae sapien elementum facilisis. Ut
+                  pharetra maximus ligula ac tempus. Phasellus luctus luctus
+                  hendrerit. Duis fermentum efficitur felis quis vehicula. Fusce
+                  ut egestas arcu, sed tristique nulla. Praesent orci magna,
+                  scelerisque in orci volutpat, condimentum pulvinar nibh.
+                  Quisque nunc mauris, tempus id ante sit amet, mollis accumsan
+                  tellus. Praesent aliquet lacinia maximus. Etiam semper enim
+                  arcu, at fringilla urna placerat in. Integer nec mollis purus,
+                  sed vehicula purus.
+                </p>
+
+                <p id="paragraph">
+                  Proin sit amet metus nibh. Quisque eleifend velit nec ligula
+                  consequat, at tempor nunc ultricies. Curabitur pulvinar a urna
+                  ut suscipit. Maecenas rutrum mi augue, quis imperdiet purus
+                  suscipit id. Vestibulum tincidunt orci eu blandit tempor.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia curae; In aliquet fermentum elit, sit
+                  amet dictum est laoreet a. Curabitur vulputate fringilla
+                  suscipit. Donec sit amet facilisis nisl. Nulla justo orci,
+                  ullamcorper eu congue vitae, ornare sed nulla. Proin turpis
+                  massa, sagittis in vestibulum quis, interdum nec nulla.
+                  Vivamus molestie eleifend rhoncus.
+                </p>
+
+                <p id="paragraph">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vivamus laoreet mattis hendrerit. Mauris eget odio id eros
+                  venenatis porttitor sed cursus dui. Ut quis felis vel risus
+                  volutpat tincidunt at a nulla. Quisque nec sollicitudin ipsum.
+                  Aenean ligula magna, porta a eleifend ullamcorper, porta sit
+                  amet arcu. Nullam dictum sodales facilisis. Vestibulum finibus
+                  volutpat nisi at volutpat. Sed id dictum magna, nec ornare
+                  ligula. In eu interdum leo. Pellentesque et convallis diam.
+                  Duis aliquet arcu sit amet orci ultrices, eu blandit nunc
+                  posuere. Vivamus dapibus, ligula consectetur volutpat
+                  elementum, arcu lorem porttitor dui, condimentum efficitur
+                  odio velit ut diam. Ut vitae eros dui. Aliquam laoreet massa
+                  elit, id placerat libero feugiat eu. Suspendisse faucibus
+                  imperdiet congue.
+                </p>
+
+                <p id="paragraph">
+                  Mauris ipsum dui, pretium vitae venenatis eu, blandit quis
+                  nibh. In hac habitasse platea dictumst. In hac habitasse
+                  platea dictumst. Aenean vehicula elementum varius. Curabitur
+                  ut tortor vel enim facilisis consequat sit amet aliquet ante.
+                  Nam feugiat ac turpis eu tincidunt. In sed laoreet sapien,
+                  condimentum tempus nibh. Duis non ipsum augue. Quisque nec
+                  lacus id leo fermentum sagittis. Interdum et malesuada fames
+                  ac ante ipsum primis in faucibus. Aenean malesuada at nibh non
+                  suscipit.
+                </p>
+
+                <p id="paragraph">
+                  Donec tortor felis, mollis in ipsum quis, iaculis rhoncus
+                  quam. Nam odio ex, rutrum sed est et, porttitor scelerisque
+                  diam. Nunc aliquam est vitae sapien elementum facilisis. Ut
+                  pharetra maximus ligula ac tempus. Phasellus luctus luctus
+                  hendrerit. Duis fermentum efficitur felis quis vehicula. Fusce
+                  ut egestas arcu, sed tristique nulla. Praesent orci magna,
+                  scelerisque in orci volutpat, condimentum pulvinar nibh.
+                  Quisque nunc mauris, tempus id ante sit amet, mollis accumsan
+                  tellus. Praesent aliquet lacinia maximus. Etiam semper enim
+                  arcu, at fringilla urna placerat in. Integer nec mollis purus,
+                  sed vehicula purus.
+                </p>
+
+                <p id="paragraph">
+                  Proin sit amet metus nibh. Quisque eleifend velit nec ligula
+                  consequat, at tempor nunc ultricies. Curabitur pulvinar a urna
+                  ut suscipit. Maecenas rutrum mi augue, quis imperdiet purus
+                  suscipit id. Vestibulum tincidunt orci eu blandit tempor.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia curae; In aliquet fermentum elit, sit
+                  amet dictum est laoreet a. Curabitur vulputate fringilla
+                  suscipit. Donec sit amet facilisis nisl. Nulla justo orci,
+                  ullamcorper eu congue vitae, ornare sed nulla. Proin turpis
+                  massa, sagittis in vestibulum quis, interdum nec nulla.
+                  Vivamus molestie eleifend rhoncus.
+                </p>
+
                 <p id="paragraph">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Vivamus laoreet mattis hendrerit. Mauris eget odio id eros
